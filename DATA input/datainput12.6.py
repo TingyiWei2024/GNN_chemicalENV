eaf0eb448a72c408a8c7a961927d508e9f5b43f8
TingyiWei2024 <twei@mun.ca>
2024-12-07 00:57:02 +0000

    Add DATA input/input 11.30.ipynb

--- DATA input/datainput12.6.py
+++ DATA input/datainput12.6.py
@@ -1,2 +1,25 @@
-print("hello")
-git add "DATA input/datainput12.6.py"
+import pandas as pd
+
+# Provide the correct path to your Excel file
+file_path = "/workspaces/GNN_chemicalENV/DATA input/CCCC.xlsx"
+
+# Load the Excel file
+data = pd.read_excel(file_path)
+
+# Display the first few rows of the data
+print(data.head())
+print(data.columns)
+
+# get nodes/edges input
+from rdkit import Chem
+
+# from dataset
+for idx, row in data.iterrows(): #.apply? 大包？
+    molecule_smiles = row ['fragment1']
+    bde_value = row['bde']
+    bdfepred_value = row['bdfe_pred']
+
+print(data.shape)  #check
+
+    
+
